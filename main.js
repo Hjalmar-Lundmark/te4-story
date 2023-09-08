5d550881506807b6aad02cf8e7cfece8cefb95e7
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-09-08 11:26:57 +0200

    Moved my story from html/main.js to story.js

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
 import './style.css'
 import star from '/star.svg'
 
-//test
 import json from './story.json'
 const story = json;
-console.log(story)
-//end test
-
-let color = "orange"; //default value for a question
 
 document.querySelector('#app').innerHTML = `
 <main class="autoWidth">
@@ -25,73 +20,37 @@ document.querySelector('#app').innerHTML = `
     </p>
     <h2>Information</h2>
     <p>
-      This story consists of interactive questions that unlocks the next parts and change how the story unfolds.
-      Answer this simple question to unlock the next part.
+      This story consists of interactive choices that unlocks the next parts and change how the story unfolds.
     </p>
 
-    <h3>What is your favorite color?</h3>
-    <input type="text" id="colorInput">
-    <button id="colorSend">Send</button>
+    <button id="startBtn">Start</button>
   </div>
 
   <div id="part1">
     <h2>The story</h2>
-    <p>
-      Hjalmar's fascination with the stars began in his childhood, when his grandfather would tell him stories
-      about
-      constellations and distant galaxies. He would spend hours with his grandfather, gazing at the night sky, and
-      dreaming of exploring the universe. As he grew older, his passion for astronomy only deepened, and he
-      decided to
-      pursue it as a career.
-      <br><br>
-      One crisp autumn evening, as Hjalmar was peering through his telescope, he spotted something extraordinary—a
-      streak of light cutting across the sky. It was a shooting star, and in that moment, he made a wish. "I wish
-      for a
-      chance to unravel the mysteries of the universe," he whispered to himself.
-      <br><br>
-      Little did Hjalmar know, his wish could come true in the most unexpected way. <br> <br>
-
-      The following morning, a important-looking, ${color} letter arrived in Hjalmar's letterbox. It was an invitation
-      from
-      the renowned Lund Observatory in Stockholm, offering him a position as a junior astronomer. It was an
-      opportunity
-      he knew he shouldn't pass up, but it would mean leaving behind the quiet comforts of Solberg. <br>
-      <br>
+    <p id="pPart1">
+      
     </p>
 
     <h3>Accept the offer?</h3>
-    <button id="wayBtn" disabled>Accept the offer and leave Solberg behind</button>
-    <button id="wayBtn2" disabled>Decline and stay in Solberg</button>
+    <button id="ques1opt1" disabled>${story[0].options[0].msg}</button>
+    <button id="ques1opt2" disabled>${story[0].options[1].msg}</button>
 
   </div>
 
 
   <div id="part2">
-    <p>
-      Hjalmar bid farewell to his family and his beloved town, embarking on a new adventure to the bustling
-      capital
-      city. The observatory was a hub of activity, filled with brilliant minds and cutting-edge equipment. Hjalmar
-      was
-      in awe of the state-of-the-art telescopes and the vast array of resources at his disposal. <br>
-      <br>
-      As the months passed, Hjalmar's dedication and enthusiasm caught the attention of his colleagues and
-      mentors. He
-      spent countless nights observing celestial phenomena, making groundbreaking discoveries, and publishing his
-      research in prestigious scientific journals. Hjalmar's reputation as an exceptional astronomer grew, and he
-      became
-      known not just in Sweden but across the international scientific community. <br>
-      <br>
-      But Hjalmar's heart was still in Solberg, and he longed for the quiet serenity of his hometown. One day, as
-      he was
-      studying a particularly enigmatic cluster of stars, he remembered his grandfather's tales of the wonders of
-      the
-      universe and the importance of sharing knowledge. <br>
-      <br>
+    <p id="pPart2">
+      
     </p>
+
+    <h3>What now?</h3>
+    <button id="q2opt1" disabled>${story[1].options[0].msg}</button>
+    <button id="q2opt2" disabled>${story[1].options[1].msg}</button>
   </div>
 
   <div id="part3">
-    <p>
+    <p id="pPart3">
       With newfound purpose, Hjalmar decided to return to Solberg. He set up a small observatory there, inviting
       local
       children and adults alike to join him in exploring the cosmos. He became a beloved figure in the town, just
@@ -124,73 +83,33 @@ document.querySelector('#app').innerHTML = `
       <a href="https://github.com/Hjalmar-Lundmark/te4-story/issues">github issue</a>
     </h3>
   </div>
-
-  <ul id="#card">
-    <li>
-      <h1>Test</h1>
-      <p>${story[0].text}</p>
-      <button>${story[0].options[0].msg}</button>
-      <button>${story[0].options[1].msg}</button>
-    </li>
-  </ul>
-
 </main>
-
-<footer>
-  <div class="autoWidth">
-    <p>
-      Site created by <a href="https://github.com/Hjalmar-Lundmark">Hjalmar Lundmark</a> |
-      <a href="https://github.com/Hjalmar-Lundmark/te4-story">Github repo</a>.<br>
-
-      Story written mainly by <a href="https://chat.openai.com/">ChatGPT</a>. <br>
-      Background photo by <a
-        href="https://unsplash.com/@vincentiu?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Vincentiu
-        Solomon</a> on <a
-        href="https://unsplash.com/photos/ln5drpv_ImI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-    </p>
-  </div>
-</footer>
 `
 
-const colorButton = document.querySelector('#colorSend')
-const colorInput = document.querySelector('#colorInput')
-const wayButton1 = document.querySelector('#wayBtn')
-const wayButton2 = document.querySelector('#wayBtn2')
+const startBtn = document.querySelector('#startBtn')
+const wayButton1 = document.querySelector('#ques1opt1')
+const wayButton2 = document.querySelector('#ques1opt2')
 
-function colorSend() {
-const part1 = document.querySelector('#part1')
-part1.style.display = "block";
+startBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  document.querySelector('#pPart1').innerHTML = story[0].text
+  const part1 = document.querySelector('#part1')
+  part1.style.display = "block";
 
-wayButton1.disabled = false;
-wayButton2.disabled = false;
-
-color = document.querySelector("#colorInput").value;
-color.toLowerCase();
-if (color !== '') {
-document.querySelector('#color').textContent = color;
-}
-}
-
-colorButton.addEventListener('click', (e) => {
-e.preventDefault();
-colorSend();
-})
-colorInput.addEventListener('keydown', (e) => {
-if (e.key === 'Enter') {
-e.preventDefault()
-colorSend();
-}
+  wayButton1.disabled = false;
+  wayButton2.disabled = false;
 })
 
 wayButton1.addEventListener('click', (e) => {
-e.preventDefault
-const end = document.querySelector('#end')
-end.style.display = 'block';
+  e.preventDefault
+  document.querySelector('#pPart2').innerHTML = story[story[0].options[0].nextId].text;
+  const part2 = document.querySelector('#part2')
+  part2.style.display = 'block';
 
 })
 wayButton2.addEventListener('click', (e) => {
-e.preventDefault
-const end = document.querySelector('#end')
-end.style.display = 'block';
-
+  e.preventDefault
+  document.querySelector('#pPart2').innerHTML = story[story[0].options[1].nextId].text;
+  const part2 = document.querySelector('#part2')
+  part2.style.display = 'block';
 })
