780d8739b853f6f86c806a91d7c6a632647bb601
Hjalmar-Lundmark <lundmarkhjalmar@gmail.com>
2023-09-08 14:46:10 +0200

    Clean up and commenting code

--- main.js
+++ main.js
@@ -22,7 +22,6 @@ document.querySelector('#app').innerHTML = `
     <p>
       This story consists of interactive choices that unlocks the next parts and change how the story unfolds.
     </p>
-
     <button id="startBtn">Start</button>
   </div>
 
@@ -34,29 +33,25 @@ document.querySelector('#app').innerHTML = `
 
     <div id="divQ1">
       <h3>Accept the offer?</h3>
-      <button id="ques1opt1" disabled>${story[0].options[0].msg}</button>
-      <button id="ques1opt2" disabled>${story[0].options[1].msg}</button>
+      <button id="ques1opt1">${story[0].options[0].msg}</button>
+      <button id="ques1opt2">${story[0].options[1].msg}</button>
     </div>
 
   </div>
 
 
   <div id="part2">
-    <p id="pPart2">
-
-    </p>
+    <p id="pPart2"></p>
 
     <div id="divQ2">
       <h3>What now?</h3>
-      <button id="ques2opt1" disabled>${story[1].options[0].msg}</button>
-      <button id="ques2opt2" disabled>${story[1].options[1].msg}</button>
+      <button id="ques2opt1">${story[1].options[0].msg}</button>
+      <button id="ques2opt2">${story[1].options[1].msg}</button>
     </div>
   </div>
 
   <div id="part3">
-    <p id="pPart3">
-
-    </p>
+    <p id="pPart3"></p>
 
     <a href="/">
       <button>Spela igen</button>
@@ -79,6 +74,7 @@ document.querySelector('#app').innerHTML = `
 </main>
 `
 
+// buttons
 const startBtn = document.querySelector('#startBtn')
 const ques1opt1Btn = document.querySelector('#ques1opt1')
 const ques1opt2Btn = document.querySelector('#ques1opt2')
@@ -87,34 +83,39 @@ const ques2opt2Btn = document.querySelector('#ques2opt2')
 
 startBtn.addEventListener('click', (e) => {
   e.preventDefault();
+
+  // shows first part
   document.querySelector('#pPart1').innerHTML = story[0].text
   const part1 = document.querySelector('#part1')
   part1.style.display = "block";
 
-  ques1opt1Btn.disabled = false;
-  ques1opt2Btn.disabled = false;
-
+  // turns off clicked button
   startBtn.disabled = true;
+  startBtn.style.border = "solid 2px black"
 })
 
 
+// First question
 let part2 = document.querySelector('#part2')
 let divQ2 = document.querySelector('#divQ2')
+// first answer
 ques1opt1Btn.addEventListener('click', (e) => {
-  e.preventDefault
+  e.preventDefault();
+  //shows part 2
   document.querySelector('#pPart2').innerHTML = story[story[0].options[0].nextId].text;
   part2.style.display = 'block';
 
-  divQ2.style.display = 'block'
-
-  ques2opt1Btn.disabled = false;
-  ques2opt2Btn.disabled = false;
-
+  // turns off clicked buttons
   ques1opt1Btn.disabled = true;
   ques1opt2Btn.disabled = true;
+
+  ques1opt1Btn.style.border = "solid 2px black"
 })
+
+// second answer
 ques1opt2Btn.addEventListener('click', (e) => {
-  e.preventDefault
+  e.preventDefault();
+  // shows part 2 and end
   document.querySelector('#pPart2').innerHTML = story[story[0].options[1].nextId].text;
   part2.style.display = 'block';
   let part3 = document.querySelector('#part3')
@@ -122,28 +123,40 @@ ques1opt2Btn.addEventListener('click', (e) => {
 
   divQ2.style.display = 'none'
 
-  ques2opt1Btn.disabled = false;
-  ques2opt2Btn.disabled = false;
-
+  // turns off clicked buttons
   ques1opt1Btn.disabled = true;
   ques1opt2Btn.disabled = true;
+
+  ques1opt2Btn.style.border = "solid 2px black"
 })
 
 
+// second question
 let part3 = document.querySelector('#part3')
+// first answer
 ques2opt1Btn.addEventListener('click', (e) => {
-  e.preventDefault
+  e.preventDefault();
+  // shows part 3 (ending)
   document.querySelector('#pPart3').innerHTML = story[story[1].options[0].nextId].text;
   part3.style.display = 'block';
 
+  // turns off clicked buttons
   ques2opt1Btn.disabled = true;
   ques2opt2Btn.disabled = true;
+
+  ques2opt1Btn.style.border = "solid 2px black"
 })
+
+// second answer
 ques2opt2Btn.addEventListener('click', (e) => {
-  e.preventDefault
+  e.preventDefault();
+  // shows part 3 (ending)
   document.querySelector('#pPart3').innerHTML = story[story[1].options[1].nextId].text;
   part3.style.display = 'block';
 
+  // turns off clicked button
   ques2opt1Btn.disabled = true;
   ques2opt2Btn.disabled = true;
+
+  ques2opt2Btn.style.border = "solid 2px black"
 })
